b1b235355b88ef53344a2112f4675a25d3d0e480
Marvin Hannemann <hannemann.marvin@gmx.de>
2023-06-08 19:47:20 +0200

    Ermöglichung von Hinzufügen von neuen Ordnern, mit Button, der Ordnersuche Dialog öffnet.

---
+++ Services/DummyFolderPathService.cs
@@ -0,0 +1,24 @@
+﻿using Dateien_Sortierprogramm.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dateien_Sortierprogramm.Services
+{
+    public class DummyFolderPathService
+    {
+        public IEnumerable<Folder> SomeFolderStrings()
+        {
+            return new Folder[]
+            {
+                new Folder() { FolderPath = "Testpfad/Ich/Habe/Keine/Ahnung" },
+                new Folder() { FolderPath = "Testpfad/Ich/Habe/Keine/Ahnung" },
+                new Folder() { FolderPath = "Testpfad/Ich/Habe/Keine/Ahnung" },
+                new Folder() { FolderPath = "Testpfad/Ich/Habe/Keine/Ahnung" },
+                new Folder() { FolderPath = "Testpfad/Ich/Habe/Keine/Ahnung" }
+            };
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,28 +1,66 @@
 ﻿using Dateien_Sortierprogramm.Data;
+using Dateien_Sortierprogramm.Services;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Navigation;
 using WPFBasics;
+
+
 
 namespace Dateien_Sortierprogramm.ViewModels
 {
     [Serializable]
     public class MainWindowViewModel : NotifyableBaseObject
     {
-        public ObservableCollection<OrderElements> ocSortingEntriesFromGrid { get; set; } = new ObservableCollection<OrderElements>();
-        public ObservableCollection<string> ocSourceFolders { get; set; } = new ObservableCollection<string>();
-
-        public ICommand AddSource { get; set; }
+        public ObservableCollection<Folder> SourceFolders { get; set; } = new ObservableCollection<Folder>();
+        public ICommand AddSourceFolderCommand { get; set; }
+        public ICommand TestHelloCommand { get; set; }
         public MainWindowViewModel()
         {
-            this.AddSource = new DelegateCommand((o) =>
-                {
-                    this.ocSourceFolders.Add("Hello");
-                });
+            //DummyFolderPathService service = new DummyFolderPathService();
+            //foreach (var folderpath in service.SomeFolderStrings())
+            //{
+            //    SourceFolders.Add(folderpath);
+            //}
+            this.TestHelloCommand = new DelegateCommand((o) =>
+            {
+                TestHelloMessageBox();
+                //MessageBox.Show("Hello World");
+               
+            });
         }
+
+
+        public void TestHelloMessageBox()
+        {
+            MessageBox.Show("Hello World");
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Title = "Ordner auswählen",
+                ValidateNames = false,
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Ordner auswählen",
+                Filter = "Ordner |*."
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                string selectedPath = Path.GetDirectoryName(dialog.FileName);
+
+                // Fügen Sie den ausgewählten Pfad zur SourceFolders-Liste hinzu
+                SourceFolders.Add(new Folder() {FolderPath = selectedPath });
+            }
+        }
+        
     }
 }
